6e7067d213ec6d0b6d3ac3309be6c3544c6aaa2f
Paventhan <paventhan.mani@gmail.com>
2025-03-22 12:34:07 +0530

    Corrected  backend block error

--- main.tf
+++ main.tf
@@ -16,10 +16,12 @@ resource "aws_instance" "pav-app-server" {
   }
 }
 
-  backend "s3" {
-    bucket         = "t2s-finops-terraform-state"
-    key            = "state/finops.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "t2s-terraform-lock"
+  terraform {
+    backend "s3" {
+      bucket         = "t2s-finops-terraform-state"
+      key            = "state/finops.tfstate"
+      region         = "us-east-1"
+      encrypt        = true
+      dynamodb_table = "t2s-terraform-lock"
+  }
   }
