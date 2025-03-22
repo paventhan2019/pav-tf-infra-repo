41fee5658c706f996a1e55a7642958d3e7cf5e57
Paventhan <paventhan.mani@gmail.com>
2025-03-22 12:38:48 +0530

    Corrected key value in  backend block

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "pav-app-server" {
   terraform {
     backend "s3" {
       bucket         = "t2s-finops-terraform-state"
-      key            = "state/finops.tfstate"
+      key            = "state/terraform.tfstate"
       region         = "us-east-1"
       encrypt        = true
       dynamodb_table = "t2s-terraform-lock"
