111ef611d76d68c270daa430d4d57104f01c30ad
Paventhan <paventhan.mani@gmail.com>
2025-03-22 12:32:09 +0530

    Added backend block

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "aws_instance" "pav-app-server" {
     Name = "pav-app-server"
   }
 }
+
+  backend "s3" {
+    bucket         = "t2s-finops-terraform-state"
+    key            = "state/finops.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "t2s-terraform-lock"
+  }
